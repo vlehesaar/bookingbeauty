bff95086666e4a6d50520f57eaa14d9a8b15bcd3
kristiinakeps <kristiina797@gmail.com>
2019-02-06 15:03:14 +0200

    Initial commit

---
+++ src/main/java/com/example/bookingbeauty/BookingbeautyApplication.java
@@ -0,0 +1,14 @@
+package com.example.bookingbeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingbeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingbeautyApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/example/bookingbeauty/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.bookingbeauty;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable();
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+
+}
